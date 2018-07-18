766c6f16f8e503b8603733610650b7fd49a378fa
saikiran <aakulasaikiran>
2018-07-18 18:47:54 +0530

    i

--- proj1/src/proj1/test1.java
+++ proj1/src/proj1/test1.java
@@ -3,7 +3,7 @@ package proj1;
 public class test1 {
 
 	public static void main(String[] args) {
-		String[] s= {"a","b","c","d","e","g","h"};
+		String[] s= {"a","b","c","d","e","g","h","i"};
 		for(int i=0;i<s.length;i++) {
 			System.out.print(s[i]+" ");
 		}
