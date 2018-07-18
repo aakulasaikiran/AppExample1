b3aade56441d7e7f1e6971ae1c770ef20ee058c7
saikiran <aakulasaikiran>
2018-07-18 18:48:46 +0530

    z x

--- proj1/src/proj1/test1.java
+++ proj1/src/proj1/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 
-		String[] s = { "a", "b", "c", "d", "e", "g", "h" };
+		String[] s = { "a", "b", "c", "d", "e", "g", "h","z","x" };
 
 		for (int i = 0; i < s.length; i++) {
 			System.out.print(s[i] + " ");
