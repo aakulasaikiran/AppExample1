06678f4926b53fc7a57a296012e465ad3bf52512
saikiran <aakulasaikiran>
2018-07-18 18:00:51 +0530

    commit2

--- proj1/src/proj1/test1.java
+++ proj1/src/proj1/test1.java
@@ -3,7 +3,7 @@ package proj1;
 public class test1 {
 
 	public static void main(String[] args) {
-		String[] s= {"a","b","c","d","e"};
+		String[] s= {"a","b","c","d","e","g"};
 		for(int i=0;i<s.length;i++) {
 			System.out.print(s[i]+" ");
 		}
