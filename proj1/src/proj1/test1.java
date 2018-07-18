3980e5086639c63c722f4a268317fbd9b724d360
saikiran <aakulasaikiran>
2018-07-18 17:48:13 +0530

    commit1

---
+++ proj1/src/proj1/test1.java
@@ -0,0 +1,13 @@
+package proj1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		String[] s= {"a","b","c","d","e"};
+		for(int i=0;i<s.length;i++) {
+			System.out.print(s[i]+" ");
+		}
+
+	}
+
+}
